4c2726d63e3505f43c1a051cfe704c7b36f1ef24
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2019-12-19 11:31:01 +0200

    Add CRUD ops on question model

--- questions/views.py
+++ questions/views.py
@@ -6,3 +6,7 @@ from rest_framework import viewsets
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
